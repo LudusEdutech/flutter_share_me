a0107e143e7a44cab4c386b0bf98c4f34ddc0728
tamirrab <tamir.rabia@gmail.com>
2020-02-17 11:13:00 +0200

    Update FlutterShareMePlugin.m
    Add WhatsApp and Facebook share

--- ios/Classes/FlutterShareMePlugin.m
+++ ios/Classes/FlutterShareMePlugin.m
@@ -10,6 +10,36 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+     if ([[call method] isEqualToString:@"shareWhatsApp"]) {
+         
+         NSString * urlWhats = [NSString stringWithFormat:@"whatsapp://send?text=%@", call.arguments[@"msg"]];
+            NSURL * whatsappURL = [NSURL URLWithString:urlWhats];
+
+if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
+    [[UIApplication sharedApplication] openURL: whatsappURL];
+    result(@"whatsappURL");
+
+}
+
+     
+  } else      if ([[call method] isEqualToString:@"shareFacebook"]) {
+
+      
+      
+
+               NSString * urlWhats = [NSString stringWithFormat:@"https://www.facebook.com/sharer/sharer.php?u=%@", call.arguments[@"msg"]];
+                  NSURL * whatsappURL = [NSURL URLWithString:urlWhats];
+
+      if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
+          [[UIApplication sharedApplication] openURL: whatsappURL];
+          result(@"shareFacebook");
+
+      }
+
+      
+      
+    }
+
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else {
